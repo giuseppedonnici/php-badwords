4230a32cedf02f2d9848927c9437de9d014a8166
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-05-03 15:00:21 +0200

    Add: strlen

--- censured.php
+++ censured.php
@@ -17,8 +17,12 @@ $para = $_GET['paragraph'];
     <p>
         <?php
         echo $para;
-        $para_explode = explode(' ', $para);
-        var_dump($para_explode);
+        ?>
+    </p>
+    <p>Lunghezza: 
+        <?php 
+        $para_len = strlen($para);
+        echo $para_len;
         ?>
     </p>
     <h2>Paragrafo censurato:</h2>
@@ -26,6 +30,12 @@ $para = $_GET['paragraph'];
         <?php
         $para_replace = str_replace($word, '***', $para);
         echo $para_replace;
+        ?>
+    </p>
+    <p>Lunghezza: 
+        <?php 
+        $para_len = strlen($para_replace);
+        echo $para_len;
         ?>
     </p>
 </body>
